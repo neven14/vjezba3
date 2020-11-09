f1bed892366c3942d1ae14334efdb9041b91276f
Neven <58472164+neven14@users.noreply.github.com>
2020-11-09 09:12:28 +0100

    Add files via upload

---
+++ VarijableVijezbe3/VarijableVijezbe3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarijableVijezbe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 32, b = 24, c = 16, d = 6;
+            double x = a * a + b * b + c * c + d * d / (a + b) * (a + b) + (c + d) * (c + d);
+            Console.WriteLine(x);
+
+
+            Console.ReadKey();        }
+    }
+}
